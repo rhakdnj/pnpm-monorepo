c00228bdbf408375f21f7aa4e461d030f398497d
rhakdnj <chungjm0711@gmail.com>
2023-12-25 22:08:15 +0900

    feat: env 파일 추가

--- apps/reservation/src/main.ts
+++ apps/reservation/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationModule } from './reservation.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
     }),
   );
   app.useLogger(app.get(Logger));
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
