ad2d830777ca0154535f60d1d04d82ed0101c16f
rhakdnj <chungjm0711@gmail.com>
2023-12-28 22:41:50 +0900

    feat: 유저가 email, password를 가지고 로그인을 한다

--- apps/auth/src/user/user.module.ts
+++ apps/auth/src/user/user.module.ts
@@ -14,5 +14,6 @@ import { UserRepository } from './user.repository';
   ],
   controllers: [UserController],
   providers: [UserService, UserRepository],
+  exports: [UserService],
 })
 export class UserModule {}
