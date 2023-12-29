a93ca3e84d7cecf83c5ae2615d1b9a9d52788112
rhakdnj <chungjm0711@gmail.com>
2023-12-29 21:22:02 +0900

    feat: env 수정

--- apps/reservation/src/reservation.module.ts
+++ apps/reservation/src/reservation.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ReservationController } from './reservation.controller';
 import { ReservationService } from './reservation.service';
-import { DatabaseModule, LoggerModule } from '@app/common';
-import { ReservationDocument, ReservationSchema } from './entity/reservation.schema';
+import { AUTH_CLIENT_TOKEN, DatabaseModule, LoggerModule } from '@app/common';
+import {
+    ReservationDocument,
+    ReservationSchema,
+} from './entity/reservation.schema';
 import { ReservationsRepository } from './reservation.repository';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
     imports: [
@@ -13,12 +17,29 @@ import * as Joi from 'joi';
             isGlobal: true,
             validationSchema: Joi.object({
                 MONGODB_URI: Joi.string().required(),
-                PORT: Joi.number().required(),
+                HTTP_PORT: Joi.number().required(),
+                AUTH_HOST: Joi.string().required(),
+                AUTH_PORT: Joi.number().required(),
             }),
         }),
         LoggerModule,
         DatabaseModule,
-        DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema }]),
+        DatabaseModule.forFeature([
+            { name: ReservationDocument.name, schema: ReservationSchema },
+        ]),
+        ClientsModule.registerAsync([
+            {
+                name: AUTH_CLIENT_TOKEN,
+                useFactory: (configService: ConfigService) => ({
+                    transport: Transport.TCP,
+                    options: {
+                        host: configService.get('AUTH_HOST'),
+                        port: configService.get('AUTH_PORT'),
+                    },
+                }),
+                inject: [ConfigService],
+            },
+        ]),
     ],
     controllers: [ReservationController],
     providers: [ReservationService, ReservationsRepository],
