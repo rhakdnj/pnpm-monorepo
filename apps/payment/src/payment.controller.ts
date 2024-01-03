398b7d93fce9b4b715ba57a96ea7b5df1365d91f
rhakdnj <chungjm0711@gmail.com>
2024-01-03 23:09:37 +0900

    feat: 공통 constant 수정

--- apps/payment/src/payment.controller.ts
+++ apps/payment/src/payment.controller.ts
@@ -1,23 +1,16 @@
-import { Controller, Get, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, UsePipes } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { CreateChargeDto } from '@app/common';
+import { DEFAULT_VALIDATION_PIPE } from '@app/common/constant/validation.pipe';
+import { PaymentCreateChargeDto } from './dto/payment-create-charge.dto';
 
 @Controller()
 export class PaymentController {
     constructor(private readonly paymentService: PaymentService) {}
 
     @MessagePattern('create_charge')
-    @UsePipes(
-        // Must use pipe directly
-        new ValidationPipe({
-            transform: true,
-            whitelist: true,
-            transformOptions: { enableImplicitConversion: true },
-            disableErrorMessages: true,
-        }),
-    )
-    async createCharge(@Payload() data: CreateChargeDto) {
+    @UsePipes(DEFAULT_VALIDATION_PIPE)
+    async createCharge(@Payload() data: PaymentCreateChargeDto) {
         return this.paymentService.createCharge(data);
     }
 }
